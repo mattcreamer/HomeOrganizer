4412df17b32ad5babedd5b1311335a0e8d763b85
mattcreamer <creamemb@bc.edu>
2023-04-23 15:21:12 -0400

    Initial Commit

---
+++ HomeOrganizer/HomeOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeOrganizerApp.swift
+//  HomeOrganizer
+//
+//  Created by Matt Creamer on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
